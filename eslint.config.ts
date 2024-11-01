3e8a0f4d0dc5b43196414554b0f95724b39efb07
imba97 <mail@imba97.cn>
2024-11-01 14:37:59 +0800

    chore: update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,3 @@
 import createConfig from '@imba97/eslint-config'
 
-export default createConfig({
-  rules: {
-    'no-console': 'off',
-    'node/prefer-global/process': 'off'
-  }
-})
+export default createConfig()
